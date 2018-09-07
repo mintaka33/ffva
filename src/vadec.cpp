5d42e0eecf3e6e7f8ea15e8bba63013f2c4167e4
mintaka33 <mintaka33@outlook.com>
2018-09-07 09:56:20 -0400

    add debug config

--- src/vadec.cpp
+++ src/vadec.cpp
@@ -13,12 +13,19 @@ extern "C" {
 
 int main(int argc, char** argv)
 {
-    AVFormatContext *input_ctx = NULL;
+    const char* hwtype = "h264_vaapi";
+    const char* infile = "/tmp/test.264";
+    AVFormatContext *input_ctx = nullptr;
     enum AVHWDeviceType type = AV_HWDEVICE_TYPE_NONE;
 
-    type = av_hwdevice_find_type_by_name("h264_vaapi");
+    type = av_hwdevice_find_type_by_name(hwtype);
     if (type == AV_HWDEVICE_TYPE_NONE)
         return -1;
+
+    if (avformat_open_input(&input_ctx, infile, nullptr, nullptr) != 0) {
+        fprintf(stderr, "Cannot open input file %s\n", infile);
+        return -1;
+    }
 
     printf("va sample decoder\n");
     return 0;
