b04452ee0dd91722ab0f93edbbbc92cfb7e77adb
mintaka33 <mintaka33@outlook.com>
2018-09-09 10:39:33 -0400

    add encoder code

---
+++ src/vaenc.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/hwcontext.h>
+}
+
+int main(int argc, char** argv)
+{
+    printf("va sample encoder\n");
+    return 0;
+}
