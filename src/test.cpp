536d75dce6351160278cef7cff24df97b5c57838
mintaka33 <mintaka33@outlook.com>
2018-09-25 20:52:58 -0400

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -7,11 +7,9 @@ int main (int argc, char** argv)
     const char* infile;
     if (argc == 1) {
          infile = "../../test/test.264";
-    }
-    else if (argc == 2) {
+    } else if (argc == 2) {
         infile = argv[1];
-    }
-    else {
+    } else {
         printf("arguments error!\n");
         return -1;
     }
